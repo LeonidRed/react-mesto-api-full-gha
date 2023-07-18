fab81fceefcc3f35ab04259e45cd0a77faf54fa3
LeonidRed <nikulin.l.m@gmail.com>
2023-07-18 19:04:05 +0300

    fix connect to mongo

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const NotFoundError = require('./errors/not-found-err')
 const errorHandler = require('./middlewares/errorHandler')
 const { requestLogger, errorLogger } = require('./middlewares/logger')
 
-const { PORT = 3001, DB_URL } = process.env
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
