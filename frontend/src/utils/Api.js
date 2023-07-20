32ed2277ff740c1f12fdff8c292df5c5e8f86111
LeonidRed <nikulin.l.m@gmail.com>
2023-07-20 20:33:37 +0300

    исправлены пути и отредактирован readme

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -74,7 +74,7 @@ class Api {
 
 
 const api = new Api({
-  baseUrl: 'http://localhost:3001',
+  baseUrl: 'http://api.imesto.nomoredomains.xyz/',
   headers: {
     authorization: `Bearer ${localStorage.getItem('token')}`,
     'Content-Type': 'application/json',
